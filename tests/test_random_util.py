74344288c6dc6c9bd58dc798cc53bc3a199031e4
newbei <hegang5862@qq.com>
2023-03-28 18:23:38 +0800

    0.02 add random_mrr_list

--- tests/test_random_util.py
+++ tests/test_random_util.py
@@ -1,5 +1,6 @@
 import random
 from banksim.util.random_util import random_car_list,random_mrr_list
+import pandas as pd
 
 def test_random_car_list():
 
@@ -22,3 +23,5 @@ def test_random_mrr_list():
 
     random.seed(3001)
     print(random_mrr_list(0.03, 240, 0.01)[236:])
+
+pd.DataFrame().to_csv()
